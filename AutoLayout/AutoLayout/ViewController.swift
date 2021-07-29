def8e075f0f12b54d615d63c4f398ca48e8dbe9a
StartingCoding <StartingCodingPartner@gmail.com>
2021-07-29 12:00:18 +0200

    Refactored ImageView and its AutoLayout constraints on the ViewController

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -8,30 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // let's avoid polluting viewDidLoad
+    // {} is referred to as closure, or anon. functions
+    let bearImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "bear_first"))
+        // this enables autolayout for our imageView
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // here's our entry point into our app
-//        view.backgroundColor = .yellow
+        view.addSubview(bearImageView)
         
-        let imageView = UIImageView(image: UIImage(named: "bear_first"))
-        view.addSubview(imageView)
-//        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        
-        // this enables autolayout for our imageView
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        
+        setupLayout()
         
     }
 
 
+    private func setupLayout() {
+        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
 }
 
