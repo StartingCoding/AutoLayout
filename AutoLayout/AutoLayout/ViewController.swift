34eb5fe156fcbf676fd9e2b09d340670ae5afa2f
StartingCoding <StartingCodingPartner@gmail.com>
2021-07-29 11:52:52 +0200

    Added imageView and AutoLayout constraints in ViewController

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -12,6 +12,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // here's our entry point into our app
+//        view.backgroundColor = .yellow
+        
+        let imageView = UIImageView(image: UIImage(named: "bear_first"))
+        view.addSubview(imageView)
+//        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        
+        // this enables autolayout for our imageView
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        
     }
 
 
