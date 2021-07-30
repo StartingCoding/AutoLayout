1833e2c1ead35b8772bc1ea54848e48655847b29
StartingCoding <StartingCodingPartner@gmail.com>
2021-07-30 11:39:07 +0200

    Fixed top image to shrink in landscape mode
    Added a a UIView half of the height of the container and placed the bear image inside that changes sizes based on the height of the view

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         let imageView = UIImageView(image: UIImage(named: "bear_first"))
         // this enables autolayout for our imageView
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
@@ -34,7 +35,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         // here's our entry point into our app
-        view.addSubview(bearImageView)
+//        view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
         
         setupLayout()
@@ -43,10 +44,32 @@ class ViewController: UIViewController {
 
 
     private func setupLayout() {
-        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        let topImageContainerView = UIView()
+        topImageContainerView.backgroundColor = .blue
+        view.addSubview(topImageContainerView)
+//        topImageContainerView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        // enable auto layout
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
+//        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        
+//        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+//        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+//        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
         descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120).isActive = true
         descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
