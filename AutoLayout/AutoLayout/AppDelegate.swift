44482f8ddf0db227457a3e25d8352b6a80790b6f
StartingCoding <StartingCodingPartner@gmail.com>
2021-08-31 09:50:57 +0200

    Created Swiping Controller based on UICollectionView

--- AutoLayout/AutoLayout/AppDelegate.swift
+++ AutoLayout/AutoLayout/AppDelegate.swift
@@ -13,7 +13,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // This is how we build out our app in code
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+//        let randomViewController = UIViewController()
+//        randomViewController.view.backgroundColor = .purple
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let swipingController = SwipingController(collectionViewLayout: layout)
+        
+        window?.rootViewController = swipingController
+        
         return true
     }
 
