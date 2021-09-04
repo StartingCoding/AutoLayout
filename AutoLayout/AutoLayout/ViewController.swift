7e1272bd560c1e33e4962cf3c2b76d9cfeaafe5f
StartingCoding <StartingCodingPartner@gmail.com>
2021-09-04 10:34:04 +0200

    Created PageControl in SwipingController

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -61,7 +61,6 @@ class ViewController: UIViewController {
         let pc = UIPageControl()
         pc.currentPage = 0
         pc.numberOfPages = 4
-//        let pinkColor = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
         pc.currentPageIndicatorTintColor = .mainPink
         pc.pageIndicatorTintColor = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
         return pc
@@ -81,27 +80,13 @@ class ViewController: UIViewController {
     }
     
     fileprivate func setupButtomControls() {
-//        view.addSubview(previousButton)
-//        previousButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
-        
-//        let yellowView = UIView()
-//        yellowView.backgroundColor = .yellow
-        
-//        let greenView = UIView()
-//        greenView.backgroundColor = .green
-        
-//        let blueView = UIView()
-//        blueView.backgroundColor = .blue
-        
         let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
         bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
         bottomControlsStackView.distribution = .fillEqually
-//        bottomControlsStackView.axis = .vertical
         
         view.addSubview(bottomControlsStackView)
         
         NSLayoutConstraint.activate([
-//            previousButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
